f7bb2ac52a79ed2b1ed30086601f689e76fad8ae
Matias Bebczuk <48242936@est.ort.edu.ar>
2024-08-04 19:02:14 -0300

    creado opcion crear usuario y lista usuarios

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,46 +1,73 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Personal_TP.Models;
+using System.Collections.Generic;
 
-namespace Personal_TP.Controllers;
-
-public class HomeController : Controller
+namespace Personal_TP.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
+        private static List<Usuario> usuarios = new List<Usuario>();
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
-    public IActionResult Clubes()
-    {
-        return View();
-    }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
 
-    public IActionResult Colegios()
-    {
-        return View();
-    }
+        public IActionResult Clubes()
+        {
+            return View();
+        }
 
-    public IActionResult Deportes()
-    {
-        return View();
+        public IActionResult Colegios()
+        {
+            return View();
+        }
+
+        public IActionResult Deportes()
+        {
+            return View();
+        }
+
+        // Acción para mostrar el formulario de creación de usuario
+        public IActionResult CrearUsuario()
+        {
+            return View();
+        }
+
+        // Acción para manejar el envío del formulario de creación de usuario
+        [HttpPost]
+        public IActionResult CrearUsuario(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                usuarios.Add(usuario);
+                return RedirectToAction("ListaUsuarios");
+            }
+            return View(usuario);
+        }
+
+        // Acción para mostrar la lista de usuarios
+        public IActionResult ListaUsuarios()
+        {
+            return View(usuarios);
+        }
     }
 }
---
+++ Models/Usuario.cs
@@ -0,0 +1,9 @@
+namespace Personal_TP.Models
+{
+    public class Usuario
+    {
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
